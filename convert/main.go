9750a78ef39d622f49a3f38a15b8d97e6ef4c1e6
lincyaw <814750204@qq.com>
2023-07-19 16:20:24 +0800

    merge 2 commands

--- convert/main.go
+++ convert/main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"os"
 	"path"
+	"path/filepath"
 	"strings"
 
 	"github.com/hhatto/gocloc"
@@ -16,6 +17,10 @@ import (
 
 	"protoc-gen-scip/scip"
 )
+
+const referenceRelStr = "reference"
+const implementationRelStr = "implementation"
+const definitionRelStr = "definition"
 
 type convertFlags struct {
 	from    string
@@ -50,10 +55,128 @@ func readFromOption(fromPath string) (*scip.Index, error) {
 	}
 	return &scipIndex, nil
 }
+
+func getTypeName(str string) string {
+	if split := strings.SplitAfter(str, "/"); len(split) > 1 {
+		return split[len(split)-1]
+	}
+	return str
+}
+
+func genDocument(d *scip.Document, index *scip.Index) []byte {
+	content := []byte(fmt.Sprintf("CREATE (:document{abspath:\"%s\", relpath:\"%s\", lang:\"%s\"});\n", filepath.Join(index.Metadata.ProjectRoot, d.RelativePath), d.RelativePath, d.Language))
+	return content
+}
+
+func genSymbol(sym *scip.SymbolInformation, d *scip.Document) []byte {
+	var create []byte
+	if strings.HasSuffix(d.RelativePath, ".proto") {
+		create = []byte(fmt.Sprintf("CREATE (:protosym{name:\"%s\", fullname:\"%s\", docrelpath:\"%s\"});\n", getTypeName(sym.Symbol), sym.Symbol, d.RelativePath))
+
+	} else {
+		create = []byte(fmt.Sprintf("CREATE (:symbol{name:\"%s\", fullname:\"%s\", docrelpath:\"%s\"});\n", getTypeName(sym.Symbol), sym.Symbol, d.RelativePath))
+
+	}
+	match := []byte(fmt.Sprintf("MATCH (n0 {relpath:\"%s\"}), (n1 {fullname:\"%s\", docrelpath:\"%s\"}) MERGE(n0)-[:contains]->(n1);\n", d.RelativePath, sym.Symbol, d.RelativePath))
+	return append(create, match...)
+}
+
+func genRelationShip(rel *scip.Relationship, sym *scip.SymbolInformation, relationStr string) []byte {
+	match := []byte(fmt.Sprintf("MATCH (n0 {fullname:\"%s\"}), (n1 {fullname:\"%s\"}) MERGE(n0)-[:%s]->(n1);\n", sym.Symbol, rel.GetSymbol(), relationStr))
+	return match
+}
+
+func genRefers(sym string, d *scip.Document) []byte {
+	contains := []byte(fmt.Sprintf("MATCH (n0 {relpath:\"%s\"}), (n1 {fullname:\"%s\"}) MERGE(n0)-[:refers]->(n1);\n", d.RelativePath, sym))
+	return contains
+}
+func convertSCIPToMemgraph(index *scip.Index) []byte {
+	content := []byte{}
+	for _, d := range index.Documents {
+		content = append(content, genDocument(d, index)...)
+		for _, s := range d.Symbols {
+			content = append(content, genSymbol(s, d)...)
+		}
+	}
+
+	for _, d := range index.Documents {
+		for _, s := range d.Symbols {
+			for _, r := range s.Relationships {
+				if r.IsDefinition {
+					content = append(content, genRelationShip(r, s, definitionRelStr)...)
+				}
+				if r.IsImplementation {
+					content = append(content, genRelationShip(r, s, implementationRelStr)...)
+				}
+				if r.IsReference {
+					content = append(content, genRelationShip(r, s, referenceRelStr)...)
+				}
+
+			}
+		}
+		for _, o := range d.Occurrences {
+			content = append(content, genRefers(o.Symbol, d)...)
+		}
+	}
+	return content
+}
+func memgraphMain(flags convertFlags) error {
+	scipIndex, err := readFromOption(flags.from)
+	if err != nil {
+		return err
+	}
+
+	var memgraphWriter io.Writer
+	toPath := flags.to
+	if toPath == "-" {
+		memgraphWriter = os.Stdout
+		// } else if !strings.HasSuffix(toPath, ".lsif") {
+		// 	return errors.Newf("expected file with .lsif extension but found %s", toPath)
+	} else {
+		graphDefinitions, err := os.OpenFile(toPath, os.O_WRONLY|os.O_CREATE, 0666)
+		defer graphDefinitions.Close()
+		if err != nil {
+			return err
+		}
+		memgraphWriter = graphDefinitions
+	}
+
+	lsifIndex := convertSCIPToMemgraph(scipIndex)
+	// if err != nil {
+	// 	return errors.Wrap(err, "failed to convert SCIP index to LSIF index")
+	// }
+
+	_, err = memgraphWriter.Write(lsifIndex)
+	if err != nil {
+		return errors.Wrapf(err, "failed to write LSIF index to path %s", toPath)
+	}
+
+	return nil
+}
+func tomemgraph() cli.Command {
+	var convertFlags convertFlags
+	convert := cli.Command{
+		Name:  "convert2cypher",
+		Usage: "Convert a SCIP index to memgrph...",
+		Flags: []cli.Flag{
+			fromFlag(&convertFlags.from),
+			&cli.StringFlag{
+				Name:        "to",
+				Usage:       "Output path for memgraph definitions",
+				Destination: &convertFlags.to,
+				Value:       "memgraph.out",
+			},
+		},
+		Action: func(c *cli.Context) error {
+			return memgraphMain(convertFlags)
+		},
+	}
+	return convert
+}
 func convertCommand() cli.Command {
 	var convertFlags convertFlags
 	convert := cli.Command{
-		Name:  "convert",
+		Name:  "convert2lsif",
 		Usage: "Convert a SCIP index to an LSIF index",
 		Flags: []cli.Flag{
 			fromFlag(&convertFlags.from),
@@ -106,7 +229,9 @@ func clocMain(flags convertFlags) error {
 	options.SkipDuplicated = true
 	var paths []string
 	root = strings.TrimPrefix(root, "file://")
-
+	if !strings.HasPrefix(root, "/") {
+		root = "/" + root
+	}
 	for _, v := range files {
 		paths = append(paths, path.Join(root, v))
 	}
@@ -183,7 +308,8 @@ func fromFlag(storage *string) *cli.StringFlag {
 func commands() []*cli.Command {
 	convert := convertCommand()
 	cloccmd := clocCommand()
-	return []*cli.Command{&convert, &cloccmd}
+	tomem := tomemgraph()
+	return []*cli.Command{&convert, &cloccmd, &tomem}
 }
 func main() {
 	app := scipApp()
